b34be89f410745a91ab31bc8489c020652a7a9dd
Pallab Jyoti Bora <pallabjyotibora75@gmail.com>
2025-06-24 19:10:52 +0530

    auth updaed

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -3,14 +3,30 @@ import ErrorHandler from "./errorMiddleware.js";
 import jwt  from "jsonwebtoken";
 import {User} from "../models/userModel.js"; 
 
-export const isAuthenticated = catchAsyncError(async (req, res, next) => {
-    const {token} = req.cookies;
-    if(!token){
-        return next(new ErrorHandler('Please login to access this resource', 401));
-    }
-    const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
+// export const isAuthenticated = catchAsyncError(async (req, res, next) => {
+//     const {token} = req.cookies;
+//     if(!token){
+//         return next(new ErrorHandler('Please login to access this resource', 401));
+//     }
+//     const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
 
-    req.user = await User.findById(decoded.id);
+//     req.user = await User.findById(decoded.id);
     
+//     next();
+// })
+
+export const isAuthenticated = catchAsyncError(async (req, res, next) => {
+  const { token } = req.cookies;
+
+  if (!token) {
+    return next(new ErrorHandler('Please login to access this resource', 401));
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
+    req.user = await User.findById(decoded.id);
     next();
-})
+  } catch (err) {
+    return next(new ErrorHandler('Invalid or expired token, please login again', 401));
+  }
+});
